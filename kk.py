4c11079bb1708991da24b461f97147f68fc5b48b
MohdEssam77 <mohammedhalboub23@gmail.com>
2025-01-12 03:37:54 +0100

    bb

--- kk.py
+++
@@ -1,9 +0,0 @@
-import fitz
-
-pdffile = r"data\03_Riegel\FT_XX_03-001_d_F.pdf"
-doc = fitz.open(pdffile)
-page = doc.load_page(0)  # number of page
-pix = page.get_pixmap()
-output = "outfile.png"
-pix.save(output)
-doc.close()
